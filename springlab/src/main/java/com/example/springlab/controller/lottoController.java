8fd529464bbedc3ed37c14f3d2324dce7c5894e3
mang0206 <alswosp0206@naver.com>
2023-10-10 12:25:06 +0900

    Study Session

--- springlab/src/main/java/com/example/springlab/controller/lottoController.java
+++ springlab/src/main/java/com/example/springlab/controller/lottoController.java
@@ -7,21 +7,33 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@SessionAttributes("lotto")
 public class lottoController {
+  @ModelAttribute("lotto")
+  public LottoDTO createLotto(){
+    return new LottoDTO();
+  }
   @RequestMapping("/lotto")
   public String lotto(@ModelAttribute("lotto")LottoDTO dto, Model model, @RequestHeader String referer){
-    int ran = new Random().nextInt(6)+1;
-    System.out.println(ran);
-
-    if(ran == dto.getLottoNum()){
-      dto.setImgName("<img src='/images/sun.png'>");
-      dto.setResult("추카추카!");
-    } else {
-      dto.setImgName("<img src='/images/cloud.png'>");
-      dto.setResult("아쉽네요.. 다음 기회를!!");
-      model.addAttribute("refinfo", referer);
+    dto.setCnt();
+    System.out.println(dto.getCnt());
+    if(dto.getCnt() >= 4){
+      dto.setImgName("snow");
+      dto.setResult("로또 응모는 낙첨된 경우에 한하여 3번 까지만 가능합니다. <br> 브라우저를 재기동한 후에 응모해 주세요.");
+    }
+    else {
+      if (new Random().nextInt(6) + 1 == dto.getLottoNum()) {
+        dto.setImgName("sun");
+        dto.setResult("추카추카!");
+        dto.setCnt(4);
+      } else {
+        dto.setImgName("cloud");
+        dto.setResult("아쉽네요.. 다음 기회를!!");
+        model.addAttribute("refinfo", referer);
+      }
     }
     return "lottoView";
   }
--- springlab/src/main/java/com/example/springlab/domain/LottoDTO.java
+++ springlab/src/main/java/com/example/springlab/domain/LottoDTO.java
@@ -6,7 +6,16 @@ import lombok.Setter;
 @Getter
 @Setter
 public class LottoDTO {
+    private int cnt;
     private int lottoNum;
     private String result;
     private String imgName;
+
+    public void setCnt() {
+        this.cnt++;
+    }
+    public void setCnt(int cnt) {
+        this.cnt = cnt;
+    }
 }
+
